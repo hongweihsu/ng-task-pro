a6a0b875cf78c8f6c45f6013bb0386c812987895
HongWei Hsu <hongweih@student.unimelb.edu.au>
2025-05-27 23:28:53 +1000

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home.component';
+import { NotFoundComponent } from './pages/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+  {
+    path: 'tasks',
+    loadChildren: () =>
+      import('./tasks/task.routes').then((m) => m.TASK_ROUTES),
+  },
+  { path: '**', component: NotFoundComponent },
+];
